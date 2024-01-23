fbbd83539bf2dc6c15054ff85b3d69055b71b387
Pablitto153 <152978150+Pablitto153@users.noreply.github.com>
2024-01-23 17:51:58 +0200

    Classes and Methods.hpp

---
+++ my_class.hpp
@@ -0,0 +1,15 @@
+#include <string>
+using namespace std;
+
+class People {
+
+public:
+
+string name;
+string surname;
+int age;
+int weight;
+
+People(string new_name, string surname, int new_age, int new_weight);
+
+};
