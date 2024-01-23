cc6b0e2c6e505d5287b0132622e794e7dd3fde91
Pablitto153 <152978150+Pablitto153@users.noreply.github.com>
2024-01-23 17:50:54 +0200

    Classes and Methods.cpp

---
+++ class_main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include "your_class.hpp"
+
+using namespace std;
+
+int main() {
+
+  People new_person("Pavel", "Abranin", 27, 82);
+  cout << new_person.name << " " << new_person.surname << endl;
+  cout << new_person.age << " years" << endl;
+  cout << new_person.weight << " Kg" << endl;
+}
